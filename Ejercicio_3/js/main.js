efdc246cec819d4bfcb5b549d7ee66a2f5a38286
Karli <karla.anhdz@gmail.com>
2022-12-12 00:29:10 -0600

    Ejercicio 3 Caso 2

--- Ejercicio_3/js/main.js
+++ Ejercicio_3/js/main.js
@@ -1,7 +1,7 @@
 
 //--------------------Ejercicio 3---------------------------------------
 
-
+//--------------------Caso 1------------------------------------
 let list3 = document.getElementById("list3");
 let btncase1 = document.getElementById("btncase1");
 let arreglo3 = [1, 22, 5, 17, 10, 5, 40, 5] ;
@@ -23,3 +23,29 @@ let arreglo3 = [1, 22, 5, 17, 10, 5, 40, 5] ;
         list3.innerHTML+= `<li class="list-group-item" >${i+1} ) ${arreglo3[i]}</li>`;
     }
     btncase1.innerHTML += Case1(arreglo3);
+
+
+
+//--------------------Caso 2------------------------------------
+
+let list4 = document.getElementById("list4");
+let btncase2 = document.getElementById("btncase2");
+let arreglo4 = [7, 41, 5, 7, 10, 13, 2] ;
+    console.log (arreglo4);
+    let numrep2 = 0;
+    function Case2(arreglo4) {
+        let repetido2 = 0;
+        let repetir2 = arreglo4[3];
+    for (let i=0; i< arreglo3.length; i++){
+        if(arreglo4[i] === arreglo4[repetir2]){
+            numrep2 ++;
+            repetido2 = arreglo4[i+1];
+     } console.log("Se repite el numero: " + repetir2)
+     return repetir2;
+    }
+}
+
+    for (let i = 0; i < arreglo4.length; i++) {
+        list4.innerHTML+= `<li class="list-group-item" >${i+1} ) ${arreglo4[i]}</li>`;
+    }
+    btncase2.innerHTML += Case2(arreglo4);
