8c2296657ea10063a68564750af6f504a33c3e0d
Karli <karla.anhdz@gmail.com>
2022-12-12 03:01:28 -0600

    Ejercicio 2 de la sección 2

--- Ejercicio1_Parte2/js/main.js
+++ Ejercicio1_Parte2/js/main.js
@@ -1,5 +1,5 @@
 
-//--------------Ejercicio 4------------------
+//--------------Ejercicio 1------------------
 
 let btncomparar1 = document.getElementById ("btncomparar1");
 
@@ -16,13 +16,16 @@ let numerito3 = document.getElementById("num3").value;
 
         if ( numerito1 > numerito2 && numerito2> numerito3) {
             elmayor = numerito1;
+            console.log ("El numero más grande es " + elmayor)
             return elmayor; 
              } else if (numerito2 > numerito1 && numerito2 > numerito3){
                 elmayor=numerito2
+                console.log ("El numero más grande es " + elmayor)
                 return elmayor;
              }
             else if (numerito3> numerito1 && numerito3 > numerito2){
              elmayor = numerito3;
+             console.log ("El numero más grande es " + elmayor)
              return elmayor;
             }
          
@@ -37,4 +40,42 @@ let numerito3 = document.getElementById("num3").value;
 
 
 
+//-------------------------Ejercicio 2---------------------------------------------
 
+let btncomparar2 = document.getElementById ("btncomparar2");
+
+
+
+let elmenor = 0;
+
+function nummenor(){
+
+let nume1 = document.getElementById("n1").value;
+let nume2 = document.getElementById("n2").value;
+let nume3 = document.getElementById("n3").value;
+
+
+        if ( nume2 > nume1 && nume3> nume1) {
+            elmenor = nume1;
+            console.log ("El numero más pequeño es " + elmenor)
+            return elmenor; 
+            
+             } else if (nume2 < nume1 && nume2 < nume3){
+                elmenor=nume2
+                console.log ("El numero más pequeño es " + elmenor)
+                return elmenor;
+             }
+            else if (nume3< nume1 && nume3 < nume2){
+             elmenor = nume3;
+             console.log ("El numero más pequeño es " + elmenor)
+             return elmenor;
+            }
+         
+
+        }
+    
+    btncomparar2.addEventListener("click", function (event) {
+        event.preventDefault();
+        let numeromaspequeño = document.getElementById("alertnumeromenor");
+        numeromaspequeño.innerHTML = `El número más pequeño es: ${nummenor(elmenor)}`;
+    })
